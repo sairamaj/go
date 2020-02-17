f78b59ab28b185474739a7c76fe3233414e341ab
sairamaj <sairamaj@gmail.com>
2020-02-16 19:38:06 -0800

    moving discs has been implemented

--- toh/game.go
+++ toh/game.go
@@ -1,28 +1,72 @@
 package main
+
 import (
 	"fmt"
 )
 
-type Disc struct{
-
-}
-type Toh struct{
-	Discs [3]Disc
+type Disc struct {
+	Size  int
 }
 
-func (t Toh) Draw(){
-	for i:=0 ; i<4; i++{	
-		fmt.Println("\t   |")
+type Tower struct {
+	discs []Disc
+}
+
+type Gamer struct {
+	towers []Tower	
+}
+
+func (t Tower) Draw() {
+	for i := 0; i < 4; i++ {
+		fmt.Println("\t        |")
 	}
 
-	d:= Disc{}
-	d.Draw()
+	for _, d := range t.discs {
+		d.Draw()
+	}
 }
 
-func (t Toh) AddDisc(disc Disc){
-	
+func (t *Tower) AddDisc(disc Disc) {
+	if t.discs == nil {
+		t.discs = []Disc{}
+	}
+
+	t.discs = append(t.discs, disc)
 }
 
-func (t Disc) Draw(){
-	fmt.Println("\t------")
+func(t *Tower) RemoveDisc() Disc{
+	topDisc := t.discs[len(t.discs)-1]
+	t.discs = t.discs[0:len(t.discs)-1]
+	return topDisc
+}
+
+func (d Disc) Draw() {
+	fmt.Print("\t")
+	for i :=0 ; i < d.Size; i++{
+		fmt.Print("-")
+	}
+	fmt.Println()
+}
+
+func CreateGamer() *Gamer {
+
+	towers := []Tower{Tower{},Tower{},Tower{}}
+	towers[0].AddDisc(Disc{Size:12})
+	towers[0].AddDisc(Disc{Size:18})
+	towers[0].AddDisc(Disc{Size:24})
+
+	g := Gamer{towers:towers}
+	return &g
+}
+
+func (g *Gamer) Draw(){
+	for _, t := range g.towers{
+		t.Draw()
+	}
+}
+
+func(g *Gamer) Move(from int , to int){
+	disc := g.towers[from-1].RemoveDisc()
+	g.towers[to-1].AddDisc(disc)
+	g.Draw()
 }
--- toh/toh.go
+++ toh/toh.go
@@ -1,8 +1,28 @@
 package main
 
-func main(){
-	towers := []Toh{Toh{},Toh{},Toh{}}
-	for _, t := range towers{
-		t.Draw()
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	ok := func(err error) {
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	g := CreateGamer()
+	g.Draw()
+	for {
+
+		var from int
+		var to int
+		fmt.Print("Plase enter from and to (ex: 1 2 ):")
+		_, err := fmt.Fscanln(os.Stdin, &from, &to)
+		ok(err)
+		fmt.Println("from:", from, "to:", to)
+		g.Move(from, to)
 	}
 }
